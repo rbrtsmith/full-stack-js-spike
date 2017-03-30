890fe9ad33464ed2c8f0b1de8c3489d751371d0a
Robert Smith <rbrtsmith84@googlemail.com>
2017-03-30 18:25:11 +0100

    full-stack-js-spike

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,37 +1,61 @@
 // @flow
+
 import 'babel-polyfill'
 
 import React from 'react'
-import { render } from 'react-dom'
+import ReactDOM from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
 import { Provider } from 'react-redux'
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import { BrowserRouter } from 'react-router-dom'
+import Immutable from 'immutable'
+import $ from 'jquery'
+import Tether from 'tether'
 
-import App from './app'
-import helloReducer from './reducer/hello'
-import { APP_CONTAINER_SELECTOR } from '../shared/config'
+import App from '../shared/app'
+import helloReducer from '../shared/reducer/hello'
+import { APP_CONTAINER_SELECTOR, JSS_SSR_SELECTOR } from '../shared/config'
 import { isProd } from '../shared/util'
+import setUpSocket from './socket'
 
-const store = createStore(combineReducers({ hello: helloReducer }),
-  // eslint-disable-next-line no-underscore-dangle
-  isProd ? undefined : window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+window.jQuery = $
+window.Tether = Tether
+require('bootstrap')
+
+/* eslint-disable no-underscore-dangle */
+const composeEnhancers = (isProd ? null : window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
+const preloadedState = window.__PRELOADED_STATE__
+/* eslint-disable no-underscore-dangle */
+
+const store = createStore(combineReducers(
+  { hello: helloReducer }),
+  { hello: Immutable.fromJS(preloadedState.hello) },
+  composeEnhancers(applyMiddleware(thunkMiddleware)))
 
 const rootEl = document.querySelector(APP_CONTAINER_SELECTOR)
 
 const wrapApp = (AppComponent, reduxStore) =>
   <Provider store={reduxStore}>
-    <AppContainer>
-      <AppComponent />
-    </AppContainer>
+    <BrowserRouter>
+      <AppContainer>
+        <AppComponent />
+      </AppContainer>
+    </BrowserRouter>
   </Provider>
 
-render(wrapApp(App, store), rootEl)
+ReactDOM.render(wrapApp(App, store), rootEl)
+const jssServerSide = document.querySelector(JSS_SSR_SELECTOR)
+// flow-disable-next-line
+jssServerSide.parentNode.removeChild(jssServerSide)
 
 if (module.hot) {
   // flow-disable-next-line
-  module.hot.accept('./app', () => {
+  module.hot.accept('../shared/app', () => {
     // eslint-disable-next-line global-require
-    const NextApp = require('./app').default
-    render(wrapApp(NextApp, store), rootEl)
+    const NextApp = require('../shared/app').default
+    ReactDOM.render(wrapApp(NextApp, store), rootEl)
   })
 }
+
+setUpSocket(store)
